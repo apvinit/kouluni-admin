5ee82bb51bdd8552b9778fc16b74c3bd7649565d
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-22 13:45:15 +0530

    add admission module, remove unneccesary component

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -4,13 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
+import { AdmissionModule } from '../../admission/admission.module';
 
 import {
   MatButtonModule,
@@ -31,16 +25,10 @@ import {
     MatInputModule,
     MatSelectModule,
     MatTooltipModule,
+    AdmissionModule
   ],
   declarations: [
-    DashboardComponent,
-    UserProfileComponent,
-    TableListComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent,
+    DashboardComponent
   ]
 })
 
