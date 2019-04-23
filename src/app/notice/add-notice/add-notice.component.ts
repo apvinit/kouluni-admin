e78e649e9ad0a184b27c59607a0d3084c113e3db
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-23 20:59:11 +0530

    create add notice component

---
+++ src/app/notice/add-notice/add-notice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-notice',
+  templateUrl: './add-notice.component.html',
+  styleUrls: ['./add-notice.component.scss']
+})
+export class AddNoticeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/notice/notice.module.ts
+++ src/app/notice/notice.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoticeRoutingModule } from './notice-routing.module';
 import { NoticeComponent } from './notice.component';
+import { AddNoticeComponent } from './add-notice/add-notice.component';
 
 @NgModule({
   imports: [
     CommonModule,
     NoticeRoutingModule
   ],
-  declarations: [NoticeComponent]
+  declarations: [NoticeComponent, AddNoticeComponent]
 })
 export class NoticeModule { }
