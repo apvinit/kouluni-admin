22d6bab4f54a4fdedfcf1463392c2e5aed67c303
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-23 21:14:35 +0530

    create notice list component

---
+++ src/app/notice/notice-list/notice-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notice-list',
+  templateUrl: './notice-list.component.html',
+  styleUrls: ['./notice-list.component.scss']
+})
+export class NoticeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/notice/notice.module.ts
+++ src/app/notice/notice.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NoticeRoutingModule } from './notice-routing.module';
 import { NoticeComponent } from './notice.component';
 import { AddNoticeComponent } from './add-notice/add-notice.component';
+import { NoticeListComponent } from './notice-list/notice-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     NoticeRoutingModule
   ],
-  declarations: [NoticeComponent, AddNoticeComponent]
+  declarations: [NoticeComponent, AddNoticeComponent, NoticeListComponent]
 })
 export class NoticeModule { }
