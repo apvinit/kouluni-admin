73148d1b312330b5a75d99392cee468707d73457
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-24 19:01:43 +0530

    create add attendance component

---
+++ src/app/attendance/add-attendance/add-attendance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-attendance',
+  templateUrl: './add-attendance.component.html',
+  styleUrls: ['./add-attendance.component.scss']
+})
+export class AddAttendanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/attendance/attendance.module.ts
+++ src/app/attendance/attendance.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AttendanceRoutingModule } from './attendance-routing.module';
 import { AttendanceComponent } from './attendance.component';
 import { AttendanceStatusComponent } from './attendance-status/attendance-status.component';
+import { AddAttendanceComponent } from './add-attendance/add-attendance.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AttendanceRoutingModule
   ],
-  declarations: [AttendanceComponent, AttendanceStatusComponent]
+  declarations: [AttendanceComponent, AttendanceStatusComponent, AddAttendanceComponent]
 })
 export class AttendanceModule { }
