0a81b81080023976d8cd8588261ef17d7052891b
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-24 21:24:14 +0530

    add timetable options and status component

--- src/app/timetable/timetable.module.ts
+++ src/app/timetable/timetable.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { TimetableRoutingModule } from './timetable-routing.module';
 import { TimetableComponent } from './timetable.component';
+import { TimetableStatusComponent } from './timetable-status/timetable-status.component';
+import { TimetableOptionsComponent } from './timetable-options/timetable-options.component';
 
 @NgModule({
   imports: [
     CommonModule,
     TimetableRoutingModule
   ],
-  declarations: [TimetableComponent]
+  declarations: [TimetableComponent, TimetableStatusComponent, TimetableOptionsComponent]
 })
 export class TimetableModule { }
