3a817b5e4de3b1ba81999e6a455dde8ceaf089dd
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-27 16:00:39 +0530

    create student model

---
+++ src/app/model/student.ts
@@ -0,0 +1,11 @@
+export interface Student {
+    id: string;
+    name: string;
+    fathername: string;
+    mothername: string;
+    dob: string;
+    class: string;
+    section: string;
+    contactNo: string;
+    address: string;
+}
