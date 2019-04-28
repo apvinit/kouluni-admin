b59c0169d95f423b17a78747d9b1f020d989e773
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-28 19:57:15 +0530

    add inner paths and refactored the parent container

--- src/app/admission/admission-routing.module.ts
+++ src/app/admission/admission-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdmissionComponent } from './admission.component';
+import { NewAdmissionComponent } from './new-admission/new-admission.component';
+import { StudentsListComponent } from './students-list/students-list.component';
 
 const routes: Routes = [
   {
-    path: '', component: AdmissionComponent
+    path: '',
+    component: AdmissionComponent,
+    children: [
+      {
+        path: 'new',
+        component: NewAdmissionComponent
+      },
+      {
+        path: 'students',
+        component: StudentsListComponent
+      },
+      {
+        path: '',
+        redirectTo: 'students',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
@@ -12,4 +30,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdmissionRoutingModule { }
+export class AdmissionRoutingModule {}
