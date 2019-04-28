577cffed79c9fe1f0d0ffceeaa3e49e1aefb3551
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-28 12:53:02 +0530

    add mat toolbar module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,11 +5,13 @@ import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    MatToolbarModule
   ],
   declarations: [
     FooterComponent,
