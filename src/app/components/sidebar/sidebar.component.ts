25a0dad194c0a52dd941c7d790e4000ab3a2604f
h4rdw1r3 <vinaugust6@gmail.com>
2019-04-28 13:06:05 +0530

    created sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,70 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
-declare const $: any;
-declare interface RouteInfo {
-  path: string;
-  title: string;
-  icon: string;
-  class: string;
-}
-export const ROUTES: RouteInfo[] = [
-  { path: '/dashboard', title: 'Dashboard', icon: 'dashboard', class: '' },
-  { path: '/admission', title: 'Admission', icon: 'people', class: '' },
-  {
-    path: '/attendance',
-    title: 'Attendance',
-    icon: 'date_range',
-    class: ''
-  },
-  {
-    path: '/timetable',
-    title: 'Timetable',
-    icon: 'watch_later',
-    class: ''
-  },
-  {
-    path: '/notice',
-    title: 'Notice',
-    icon: 'notifications',
-    class: ''
-  },
-  {
-    path: '/events',
-    title: 'Events',
-    icon: 'events',
-    class: ''
-  },
-  {
-    path: '/fees',
-    title: 'Fee Collection',
-    icon: 'money',
-    class: ''
-  },
-  {
-    path: '/upgrade',
-    title: 'Upgrade to PRO',
-    icon: 'unarchive',
-    class: 'active-pro'
-  }
-];
-
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  menuItems: any[];
+
+  links = [
+    { url: '', title: 'Home'},
+    { url: '/dashboard', title: 'Dashboard'},
+    { url: '/admission', title: 'Admission'},
+    { url: '/attendance', title: 'Attendance'},
+    { url: '/timetable', title: 'Timetable'},
+    { url: '/notice', title: 'Notice'},
+    { url: '/events', title: 'Events'},
+    { url: '/holiday', title: 'Holidays'},
+  ];
 
   constructor() {}
 
-  ngOnInit() {
-    this.menuItems = ROUTES.filter(menuItem => menuItem);
-  }
-  isMobileMenu() {
-    if ($(window).width() > 991) {
-      return false;
-    }
-    return true;
-  }
+  ngOnInit() {}
 }
