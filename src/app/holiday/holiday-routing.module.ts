77132121c1db148c5bcfcaaf9c32ded4656614c7
h4rdw1r3 <vinaugust6@gmail.com>
2019-05-04 19:33:17 +0530

    add internal routes for holiday module

--- src/app/holiday/holiday-routing.module.ts
+++ src/app/holiday/holiday-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HolidayComponent } from './holiday.component';
+import { HolidayListComponent } from './holiday-list/holiday-list.component';
+import { AddEditHolidayComponent } from './add-edit-holiday/add-edit-holiday.component';
 
 const routes: Routes = [
   {
-    path: '', component: HolidayComponent
+    path: '',
+    component: HolidayComponent,
+    children: [
+      {
+        path: 'list',
+        component: HolidayListComponent
+      },
+      {
+        path: 'new',
+        component: AddEditHolidayComponent
+      },
+      {
+        path: 'edit/:id',
+        component: AddEditHolidayComponent
+      },
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
@@ -12,4 +34,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HolidayRoutingModule { }
+export class HolidayRoutingModule {}
