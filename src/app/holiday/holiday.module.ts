0775462d71518103bc14671b113196addf4ffa7f
h4rdw1r3 <vinaugust6@gmail.com>
2019-05-04 19:57:37 +0530

    add material module and reative forms module

--- src/app/holiday/holiday.module.ts
+++ src/app/holiday/holiday.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { HolidayRoutingModule } from './holiday-routing.module';
 import { HolidayComponent } from './holiday.component';
 import { MaterialModule } from 'app/material.module';
+import { HolidayListComponent } from './holiday-list/holiday-list.component';
+import { AddEditHolidayComponent } from './add-edit-holiday/add-edit-holiday.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HolidayComponent],
+  declarations: [HolidayComponent, HolidayListComponent, AddEditHolidayComponent],
   imports: [
     CommonModule,
     HolidayRoutingModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class HolidayModule { }
