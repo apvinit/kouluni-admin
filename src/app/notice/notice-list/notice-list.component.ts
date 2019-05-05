579958671fbee9fcea5de64deabbb9dd116c977a
h4rdw1r3 <vinaugust6@gmail.com>
2019-05-05 13:52:47 +0530

    implement delete and edit methods

--- src/app/notice/notice-list/notice-list.component.ts
+++ src/app/notice/notice-list/notice-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Notice } from 'app/model/notice';
+import { FirebaseService } from 'app/api/firebase.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-notice-list',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notice-list.component.scss']
 })
 export class NoticeListComponent implements OnInit {
+  notices$: Observable<Notice[]>;
+  columnsToDisplay = ['title', 'description', 'actions'];
 
-  constructor() { }
+  constructor(
+    private firebaseService: FirebaseService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.notices$ = this.firebaseService.getNotices();
   }
 
+  editNotice(id: string) {
+    this.router.navigate(['../edit', id], { relativeTo: this.route})
+  }
+
+  deleteNotice(id: string) {
+    this.firebaseService.deleteNotice(id);
+  }
 }
