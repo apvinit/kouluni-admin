7d4d5b40b17d9473369e45fa02b58d8e0a1ce507
h4rdw1r3 <vinaugust6@gmail.com>
2019-05-05 14:03:10 +0530

    Import material module and material module

--- src/app/attendance/attendance.module.ts
+++ src/app/attendance/attendance.module.ts
@@ -5,11 +5,15 @@ import { AttendanceRoutingModule } from './attendance-routing.module';
 import { AttendanceComponent } from './attendance.component';
 import { AttendanceStatusComponent } from './attendance-status/attendance-status.component';
 import { AddAttendanceComponent } from './add-attendance/add-attendance.component';
+import { MaterialModule } from 'app/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    AttendanceRoutingModule
+    AttendanceRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
   declarations: [AttendanceComponent, AttendanceStatusComponent, AddAttendanceComponent]
 })
