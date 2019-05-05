5d95d591dfee6527590cf1086cbb33df03518ae0
h4rdw1r3 <vinaugust6@gmail.com>
2019-05-05 12:07:14 +0530

    add notice module routing paths
    new, list and the default path

--- src/app/notice/notice-routing.module.ts
+++ src/app/notice/notice-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NoticeComponent } from './notice.component';
+import { AddNoticeComponent } from './add-notice/add-notice.component';
+import { NoticeListComponent } from './notice-list/notice-list.component';
 
 const routes: Routes = [
   {
-    path: '', component: NoticeComponent
+    path: '',
+    component: NoticeComponent,
+    children: [
+      {
+        path: 'new',
+        component: AddNoticeComponent
+      },
+      {
+        path: 'list',
+        component: NoticeListComponent
+      },
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
