b5065f4a7221076314bca88e67f156347ac72d82
h4rdw1r3 <vinaugust6@gmail.com>
2019-05-05 16:14:29 +0530

    add material module and reactive forms module

--- src/app/timetable/timetable.module.ts
+++ src/app/timetable/timetable.module.ts
@@ -5,12 +5,20 @@ import { TimetableRoutingModule } from './timetable-routing.module';
 import { TimetableComponent } from './timetable.component';
 import { TimetableStatusComponent } from './timetable-status/timetable-status.component';
 import { TimetableOptionsComponent } from './timetable-options/timetable-options.component';
+import { MaterialModule } from 'app/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    TimetableRoutingModule
+    TimetableRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
-  declarations: [TimetableComponent, TimetableStatusComponent, TimetableOptionsComponent]
+  declarations: [
+    TimetableComponent,
+    TimetableStatusComponent,
+    TimetableOptionsComponent
+  ]
 })
-export class TimetableModule { }
+export class TimetableModule {}
