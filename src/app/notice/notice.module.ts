0e19d88bb26ef5bf1350ee87c4588691f86ff269
h4rdw1r3 <vinaugust6@gmail.com>
2019-05-05 12:08:55 +0530

    Import material module and reactive forms module

--- src/app/notice/notice.module.ts
+++ src/app/notice/notice.module.ts
@@ -4,11 +4,15 @@ import { NoticeRoutingModule } from './notice-routing.module';
 import { NoticeComponent } from './notice.component';
 import { AddNoticeComponent } from './add-notice/add-notice.component';
 import { NoticeListComponent } from './notice-list/notice-list.component';
+import { MaterialModule } from 'app/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    NoticeRoutingModule
+    NoticeRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
   declarations: [NoticeComponent, AddNoticeComponent, NoticeListComponent]
 })
