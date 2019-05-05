ec1265311c16baf705796ae22ae548faba8c682a
h4rdw1r3 <vinaugust6@gmail.com>
2019-05-05 10:29:49 +0530

    implement update functionality

--- src/app/holiday/add-edit-holiday/add-edit-holiday.component.ts
+++ src/app/holiday/add-edit-holiday/add-edit-holiday.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { FirebaseService } from 'app/api/firebase.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Holiday } from 'app/model/holiday';
 
 @Component({
   selector: 'app-add-edit-holiday',
@@ -8,23 +10,46 @@ import { FirebaseService } from 'app/api/firebase.service';
   styleUrls: ['./add-edit-holiday.component.scss']
 })
 export class AddEditHolidayComponent implements OnInit {
-
   holidayForm = new FormGroup({
     name: new FormControl(''),
     date: new FormControl(''),
     description: new FormControl('')
   });
+  update = false;
+  holidayId = '';
 
   @ViewChild('holidayFormTemplate') holidayFormTemplate;
 
-  constructor(private firebaseService: FirebaseService) { }
+  constructor(
+    private firebaseService: FirebaseService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.holidayId = this.route.snapshot.paramMap.get('id');
+    if (this.holidayId !== null) {
+      this.update = true;
+      this.firebaseService.getHolidayById(this.holidayId).subscribe(holiday => {
+        this.holidayForm.setValue({
+          name: holiday.name,
+          date: holiday.date,
+          description: holiday.description
+        });
+      });
+    }
   }
 
   onSubmit() {
-    this.firebaseService.addHoliday(this.holidayForm.value);
-    this.resetForm();
+    const holiday: Holiday = this.holidayForm.value;
+    if (this.update) {
+      holiday.id = this.holidayId;
+      this.firebaseService.updateHoliday(holiday);
+      this.router.navigate(['../..'], { relativeTo: this.route });
+    } else {
+      this.firebaseService.addHoliday(holiday);
+      this.resetForm();
+    }
   }
 
   resetForm() {
@@ -32,4 +57,7 @@ export class AddEditHolidayComponent implements OnInit {
     this.holidayFormTemplate.resetForm();
   }
 
+  goToHolidays() {
+    this.router.navigate(['/holiday']);
+  }
 }
