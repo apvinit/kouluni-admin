5d5a0439ceba77f407d3947bf95a15f459a8334a
h4rdw1r3 <vinaugust6@gmail.com>
2019-05-06 20:28:39 +0530

    recreate template with angular material and dummy data
    remove old code and recreate the template with dummy data and
    use angular material card and table to organise items

--- src/app/timetable/timetable-status/timetable-status.component.ts
+++ src/app/timetable/timetable-status/timetable-status.component.ts
@@ -6,10 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timetable-status.component.scss']
 })
 export class TimetableStatusComponent implements OnInit {
+  columnsToDisplay = ['day', 'startTime', 'endTime', 'subject', 'teacher'];
 
-  constructor() { }
+  timetables = [
+    {
+      day: 'monday',
+      startTime: '07:00 am',
+      endTime: '07:40 am',
+      subject: 'Maths',
+      teacher: 'Amitabh Bachhan'
+    },
+    {
+      day: 'tuesday',
+      startTime: '07:00 am',
+      endTime: '07:40 am',
+      subject: 'Maths',
+      teacher: 'Amitabh Bachhan'
+    },
+    {
+      day: 'wednesday',
+      startTime: '07:00 am',
+      endTime: '07:40 am',
+      subject: 'Maths',
+      teacher: 'Amitabh Bachhan'
+    },
+    {
+      day: 'thursday',
+      startTime: '07:00 am',
+      endTime: '07:40 am',
+      subject: 'Maths',
+      teacher: 'Amitabh Bachhan'
+    },
+    {
+      day: 'friday',
+      startTime: '07:00 am',
+      endTime: '07:40 am',
+      subject: 'Maths',
+      teacher: 'Amitabh Bachhan'
+    },
+    {
+      day: 'saturday',
+      startTime: '07:00 am',
+      endTime: '07:40 am',
+      subject: 'Maths',
+      teacher: 'Amitabh Bachhan'
+    }
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
